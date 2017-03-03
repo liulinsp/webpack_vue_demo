def4a3a0ff621c375151d91b05cf6bd376e7da2b
liu_lin_sp <liu_lin_sp@126.com>
2017-03-03 13:27:48 +0800

    提交

---
+++ app/index/components/Favlist.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+	<li v-for="n in 10">div</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style>
+    html{
+        background: #fff;
+    }
+</style>
